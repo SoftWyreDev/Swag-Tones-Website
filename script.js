c1b5c8d0f787a49f9267dcb9a9d205a3945d6beb
Alex <bahler831@gmail.com>
2026-04-03 22:56:09 -0700

    Email/phone obfuscation and link styling

---
+++ script.js
@@ -0,0 +1,19 @@
+window.addEventListener("load", (event) => {
+    const emailLinks = document.querySelectorAll('a[href^="mailto:"]');
+    emailLinks.forEach(link => {
+        console.log(link.innerText);
+        link.href = link.href + link.innerText;
+    });
+});
+
+window.addEventListener("load", () => {
+    const phoneLink = document.querySelector('.book-phone');
+
+    const encodedNum = "MTgzMTI5NTkxOTE="; 
+    const number = atob(encodedNum);
+
+    const formatted = `(${number.slice(1, 4)}) ${number.slice(4, 7)}-${number.slice(7)}`;
+
+    phoneLink.innerHTML += formatted;
+    phoneLink.href = `tel:${number}`;
+});
