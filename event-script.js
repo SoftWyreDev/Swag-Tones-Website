7ec8f850f10eac526440c039bcfc391e799f10fb
Alex <bahler831@gmail.com>
2026-05-26 23:33:26 -0700

    updated events

--- event-script.js
+++ event-script.js
@@ -1,16 +1,14 @@
 // Event List
 const data = `
-Saturday, April 11, 2026 | 7:00 - 10:00 p.m. | Sevy's Bar + Kitchen | 7500 Old Dominion Ct, Aptos, CA
-
-Friday, April 17, 2026 | 6:45 - 9:15 p.m. | Santa Cruz Yacht Club | 244 4th Ave, Santa Cruz, CA
-
-Friday, April 24, 2026 | 7:00 - 10:00 p.m. | Vino by the Sea | 55 Municipal Wharf Ste B, Santa Cruz, CA
-
-Saturday, May 16, 2026 | 5:00 - 8:00 p.m. | Swag Tones Appreciation Event Swag Shop |  13026 Central Ave, Boulder Creek, CA, 95006
-
-Thursday, May 21, 2026 | 6:00 - 8:00 p.m. | Bargetto Winery | 3535 N Main St, Soquel, CA 95073
-
 Friday, May 29, 2026 | 5:00 - 8:00 p.m. | Skypark Food Trucks-a-go-go | 361 Kings Village Rd, Scotts Valley, CA 95066
+
+Friday, June 6, 2026 | 9:00 - 11:59 p.m. | The Sand Bar |  211 Esplanade, Capitola, CA 95010
+
+Saturday, June 20, 2026 | 5:00 - 8:00 p.m. | Private Event | Watsonville
+
+Sunday, June 21, 2026 | 4:00 - 7:00 p.m. | Fathers Day at El Vaquero Winery Tasting Room | 2901 Freedom Blvd, Corralitos, CA 95076
+
+Saturday, June 27, 2026 | 10:00 - 11:59 p.m. | Fernwood Campground and Resort | 47200 CA-1, Big Sur, CA 93920
 `;
 
 const container = document.getElementById("events");
