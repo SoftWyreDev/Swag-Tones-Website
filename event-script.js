b3583e8ff392ffbfeaefab2c79cb0f35861422b4
Alex <bahler831@gmail.com>
2026-06-25 18:31:29 -0700

    Updated events 6/25/26

--- event-script.js
+++ event-script.js
@@ -1,14 +1,17 @@
 // Event List
 const data = `
-Friday, May 29, 2026 | 5:00 - 8:00 p.m. | Skypark Food Trucks-a-go-go | 361 Kings Village Rd, Scotts Valley, CA 95066
-
-Friday, June 6, 2026 | 9:00 - 11:59 p.m. | The Sand Bar |  211 Esplanade, Capitola, CA 95010
-
-Saturday, June 20, 2026 | 5:00 - 8:00 p.m. | Private Event | Watsonville
-
-Sunday, June 21, 2026 | 4:00 - 7:00 p.m. | Fathers Day at El Vaquero Winery Tasting Room | 2901 Freedom Blvd, Corralitos, CA 95076
 
 Saturday, June 27, 2026 | 10:00 - 11:59 p.m. | Fernwood Campground and Resort | 47200 CA-1, Big Sur, CA 93920
+
+Monday, June 29, 2026 | 6:00 - 9:00 p.m. | The Sand Bar |  211 Esplanade, Capitola, CA 95010
+
+Friday, July 10, 2026 | 7:00 - 10:00 p.m. | SEVYs Bar and Kitchen | 7500 Old Dominion Ct, Aptos, CA 95003
+
+Sunday, July 19, 2026 | 2:00 - 4:00 p.m. | Art and Wine Festival at Bargetto Winery | 3535 N Main St, Soquel, CA 95073
+
+Friday, July 24, 2026 | 6:00 - 8:30p.m. | Four Points by Sheraton Presents Music Under The Stars | 5030 Scotts Valley Dr, Scotts Valley, CA 95066
+
+Saturday, July 31, 2026 | 7:00 - 10:00 p.m. | Bruno’s Bar and Grill | 230 Mt Hermon Rd, Scotts Valley, CA 95066
 `;
 
 const container = document.getElementById("events");
