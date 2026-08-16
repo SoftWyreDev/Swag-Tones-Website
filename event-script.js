3719ec8c11b22e56129c8ce0eeaa9d5ac04ff33a
Alex <bahler831@gmail.com>
2026-08-16 10:36:05 -0700

    Updated events 8/16/26

--- event-script.js
+++ event-script.js
@@ -1,10 +1,6 @@
 // Event List
 const data = `
-Saturday, Aug 8, 2026 | 7:00 - 10:00 p.m. | SEVYs Bar and Kitchen | 7500 Old Dominion Ct, Aptos, CA 95003
-
-Friday, Aug 14, 2026 | 6:00 - 8:00p.m. | Steel Bonnet Brewing Company | 20 Victor Square Ext B, Scotts Valley, CA 95066
-
-Sunday, Aug 19, 2026 | 7:45 - 10:00 p.m. | Bay Bar and Grill Twilight After Party | 211 Esplanade, Capitola, CA 95010
+Wednesday, Aug 19, 2026 | 7:45 - 10:00 p.m. | Bay Bar and Grill Twilight After Party | 211 Esplanade, Capitola, CA 95010
 
 Friday, Aug 21, 2026 | 9:00 - 12:00 a.m. | The Crows Nest Restaurant  | 2218 E. Cliff Dr, Santa Cruz, CA 95062
 
@@ -15,6 +11,8 @@ Saturday, Aug 29, 2026 | 8:30 - 11:30 p.m. | The Sand Bar |  211 Esplanade, Capi
 Friday, Sept 4, 2026 | 8:00 - 11:00 p.m. | The Catalyst Atrium | 1011 Pacific Ave, Santa Cruz, CA 95060
 
 Friday, Sept 11, 2026 | 7:00 - 10:00 p.m. | Vino By The Sea | 55 Municipal Wharf Ste B, Santa Cruz, CA 95060
+
+Saturday, Sept 12, 2026 | 5:30 - 8:30 p.m. | Private Party | Graham Hill Road, Santa Cruz, CA 95060
 
 Sunday, Sept 20, 2026 | 1:00 - 4:00 p.m. | Miramar Vineyards | 12255 New Ave, San Martin, CA 95046
 
