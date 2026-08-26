473cf39e98498ebb1c517a7c746428fbb88fe7b4
Alex <bahler831@gmail.com>
2026-08-26 13:02:09 -0700

    Updated events 8/26/26

--- event-script.js
+++ event-script.js
@@ -1,17 +1,8 @@
 // Event List
 const data = `
-
-Friday, Aug 21, 2026 | 8:00 - 11:00 p.m. | The Crows Nest Restaurant  | 2218 E. Cliff Dr, Santa Cruz, CA 95062
-
-Saturday, Aug 22, 2026 | 1:00 - 4:00 p.m. | KBAA Wines Pre-Harvest Party | 1900 Glen Canyon Road, Santa Cruz, CA 95060
-
 Saturday, Aug 29, 2026 | 8:30 - 11:30 p.m. | The Sand Bar |  211 Esplanade, Capitola, CA 95010
 
-Friday, Sept 4, 2026 | 8:00 - 11:00 p.m. | The Catalyst Atrium | 1011 Pacific Ave, Santa Cruz, CA 95060
-
 Friday, Sept 11, 2026 | 7:00 - 10:00 p.m. | Vino By The Sea | 55 Municipal Wharf Ste B, Santa Cruz, CA 95060
-
-Saturday, Sept 12, 2026 | 5:30 - 8:30 p.m. | Private Party | Graham Hill Road, Santa Cruz, CA 95060
 
 Sunday, Sept 20, 2026 | 1:00 - 4:00 p.m. | Miramar Vineyards | 12255 New Ave, San Martin, CA 95046
 
